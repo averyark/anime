a062c8ae43d6b6b571b6e3be8d5750c01a7870c2
DevArke <90465200+DevArke@users.noreply.github.com>
2022-06-27 18:03:27 +0800

    Test

--- src/client/interface/test.lua
+++ src/client/interface/test.lua
@@ -5,6 +5,21 @@
     Created 	> 17/06/2022
 --]]
 
+local UserInputService = game:GetService('UserInputService')
+local RunService = game:GetService('RunService')
+local Players = game:GetService('Players')
+local StarterGui = game:GetService('StarterGui')
+local StarterPlayer = game:GetService('StarterPlayer')
+local ReplicatedFirst = game:GetService('ReplicatedFirst')
+local ContentProvider = game:GetService('ContentProvider')
+local ContextActionService = game:GetService('ContextActionService')
+local Lighting = game:GetService('Lighting')
+local SoundService = game:GetService('SoundService')
+local SocialService = game:GetService('SocialService')
+local CollectionService = game:GetService('CollectionService')
+local MarketplaceService = game:GetService('MarketplaceService')
+local PolicyService = game:GetService('PolicyService')
+local TextService = game:GetService('TextService')
 local ReplicatedStorage = game:GetService('ReplicatedStorage')
 
 local Knit = require(ReplicatedStorage.Packages.Knit)
@@ -19,9 +34,8 @@ local test = Knit.CreateController {
     Name = "test";
     Client = {};
 }
+utilities.ui.get("test"):observe(function(ui)
 
-utilities.ui.get("Test"):observe(function(ui)
-        
 end)
 
 function test:KnitStart()
