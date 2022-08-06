d83a76f8f173f4dda86dff293e6315a389d6c1d0
DevArke <90465200+DevArke@users.noreply.github.com>
2022-08-06 22:46:16 +0800

    sync

---
+++ src/server/path_manager.lua
@@ -0,0 +1,39 @@
+--[[
+    FileName	> test.lua
+    Author  	> AveryArk
+    Contact 	> Twitter: https://twitter.com/averyark_
+    Created 	> 16/07/2022
+--]]
+
+local RunService = game:GetService("RunService")
+local Players = game:GetService("Players")
+local StarterPlayer = game:GetService("StarterPlayer")
+local ReplicatedFirst = game:GetService("ReplicatedFirst")
+local Lighting = game:GetService("Lighting")
+local CollectionService = game:GetService("CollectionService")
+local MarketplaceService = game:GetService("MarketplaceService")
+local ServerScriptService = game:GetService("ServerScriptService")
+local ServerStorage = game:GetService("ServerStorage")
+local MessagingService = game:GetService("MessagingService")
+local MemoryStoreService = game:GetService("MemoryStoreService")
+local BadgeService = game:GetService("BadgeService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local LocalizationService = game:GetService("LocalizationService")
+
+local Knit = require(ReplicatedStorage.Packages.Knit)
+local Janitor = require(ReplicatedStorage.Packages.Janitor)
+local Promise = require(ReplicatedStorage.Packages.Promise)
+local t = require(ReplicatedStorage.Packages.t)
+local Signal = require(ReplicatedStorage.Packages.Signal)
+local TableUtil = require(ReplicatedStorage.Packages.TableUtil)
+local utilities = require(ReplicatedStorage.utilities)
+local matter = require(ReplicatedStorage.Packages.matter)
+
+local test = Knit.CreateService({
+	Name = "test",
+	Client = {},
+})
+
+function test:KnitStart() end
+
+return test
